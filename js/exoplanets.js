805feaf6eb6a5b6465615ff59e4439d4fbfaed57
SJB <StevenJBurns@users.noreply.github.com>
2017-08-21 06:35:27 -0600

    OOP and canvas animation; Classes for star, planet and first progress on visuals

--- js/exoplanets.js
+++ js/exoplanets.js
@@ -1,56 +1,144 @@
 "use strict"
 
+// Find the canvas via the DOM and get a 2D context to it
 let canvas = document.getElementById("canvasSystem");
 let ctx = canvas.getContext("2d");
 
-fillCanvas();
-drawStar();
-drawPlanet();
-drawPlanet();
+// function drawOrbit(radius) {
+//   ctx.strokeStyle = "#373737";
+//   ctx.beginPath();
+//   ctx.arc(320, 320, radius, 0, Math.PI*2, true);
+//   ctx.closePath();
+//   ctx.stroke();
+// }
 
-function fillCanvas(){
-  ctx.fillStyle = "black";   //rgb(randomR, randomG, randomB);
+// Classes -- SolarSystem class is a container class for StellarObject objects
+//  -- Star, Planet and Moon inherit from superclass StellarObject
+
+class SolarSystem {
+  // this.star = new Star();
+  // let planets = [];
+
+  constructor(star, planets){
+
+  }
+}
+
+class StellarObject {
+  constructor(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+}
+
+class Star extends StellarObject {
+  constructor(){
+    super();
+  }
+  draw(){
+    let g = ctx.createRadialGradient(320,320,4,320,320,32);
+    g.addColorStop(0, "#FFFF99");
+    g.addColorStop(0.05, "#FFFF99")
+    g.addColorStop(1, "#000000");
+
+    ctx.fillStyle = g;
+    // ctx.beginPath();
+    // ctx.arc(320, 320, 24, 0, Math.PI*2, true);
+    // ctx.closePath();
+    ctx.fillRect(0,0,640,640);
+  }
+
+  update(){
+
+  }
+}
+
+class Planet extends StellarObject {
+  constructor(){
+    super();
+
+    this.theta = Math.random() * 2 * Math.PI;
+    this.dtheta = Math.random() / 25;
+    this.radius = Math.floor(Math.random() * 240) + 32;
+
+    this.x = (Math.cos(this.theta) * this.radius) + 320;
+    this.y = (Math.sin(this.theta) * this.radius) + 320;
+
+    this.trailLength = 64;
+    this.trailPositions = [];
+  }
+
+  draw(){
+    this.update();
+    //this.drawOrbit(this.radius);
+    this.drawTrail();
+
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 4, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  drawOrbit(r) {
+    ctx.strokeStyle = "#3F3F3F";
+    ctx.beginPath();
+    ctx.arc(320, 320, this.radius, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  drawTrail(){
+    let opacity = 0;
+
+    for (let trail of this.trailPositions){
+
+      ctx.strokeStyle = `rgba(255,255,255,${opacity})`;
+      ctx.beginPath();
+      ctx.arc(trail.x, trail.y, 1, this.theta, this.theta - 0.1, true);
+      ctx.stroke();
+
+      opacity += 0.5/this.trailLength;
+    }
+  }
+
+  update(){
+    if (this.theta >= (2 * Math.PI)) this.theta = 0;
+
+    this.trailPositions.push({x: this.x, y: this.y});
+    if (this.trailPositions.length > this.trailLength) this.trailPositions.shift();
+
+    this.x = (Math.cos(this.theta) * this.radius) + 320;
+    this.y = (Math.sin(this.theta) * this.radius) + 320;
+
+    this.theta += this.dtheta;
+  }
+}
+
+class Moon extends StellarObject {
+  constructor(){
+    super();
+  }
+
+}
+
+// Test Star & Planet
+let star = new Star();
+let p1 = new Planet();
+let p2 = new Planet();
+
+// Animation of the Canvas element
+function animateCanvas(){
+  window.requestAnimationFrame(animateCanvas);
+
+  ctx.fillStyle = "black";
   ctx.fillRect(0,0,640,640);
+
+  star.draw();
+  p1.draw();
+  p2.draw();
 }
 
-function drawStar(){
-ctx.fillStyle = createRandomColorRGB()
-  ctx.beginPath();
-  ctx.arc(320, 320, 24, 0, Math.PI*2, true);
-  ctx.closePath();
-  ctx.fill();
-}
-
-function drawPlanet(){
-  ctx.fillStyle = "white";       //createRandomColorRGB();
-  let randomTheta = Math.random() * 2 * Math.PI;
-  let randomRadius = Math.floor(Math.random() * 320) + 32;
-
-  let x = (Math.cos(randomTheta) * randomRadius) + 320;
-  let y = (Math.sin(randomTheta) * randomRadius) + 320;
-
-  ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2, true);
-  ctx.closePath();
-  ctx.fill();
-
-  drawOrbit(randomRadius);
-}
-
-function drawOrbit(radius) {
-  ctx.strokeStyle = "white";
-  ctx.beginPath();
-  ctx.arc(320, 320, radius, 0, Math.PI*2, true);
-  ctx.closePath();
-  ctx.stroke();
-}
-
-function createRandomColorRGB(){
-  let randomR = Math.floor(Math.random() * 255);
-  let randomG = Math.floor(Math.random() * 255);
-  let randomB = Math.floor(Math.random() * 255);
-
-  console.log(randomR, randomG, randomB);
-
-  return `rgb(${randomR},${randomG},${randomB})`
-}
+// Start the canvas animation; recursively calls window.requestAnimationFrame()
+animateCanvas();
