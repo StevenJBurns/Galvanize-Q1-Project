bfed45d085a7eb4705a9af929c9ee139dd14adad
SJB <StevenJBurns@users.noreply.github.com>
2017-08-25 21:34:03 -0600

    Orbit overlays and actual planet motion fixed!  Using 2 * semiMajor to shift them to the right of the system star

--- js/exoplanets.js
+++ js/exoplanets.js
@@ -39,17 +39,10 @@ class Star extends StellarObject {
   constructor(){
     super();
 
-    // known parameters of an elliptic orbit found in the NASA database
-    this.ecc = 0.75;
-    this.semiMajor = Math.floor(Math.random() * 240);
-    this.semiMinor = Math.sqrt(Math.pow(this.semiMajor, 2) * (1 - Math.pow(this.ecc, 2)));
-
-    //foci
-    this.foci = Math.sqrt(Math.pow(this.semiMajor, 2) - Math.pow(this.semiMinor, 2));
   }
 
   draw(){
-    let g = ctx.createRadialGradient(320 - this.foci, 320, 4, 320 - this.foci, 320, 32);
+    let g = ctx.createRadialGradient(320, 320, 4, 320, 320, 32);
     g.addColorStop(0, "#FFFF99");
     g.addColorStop(0.05, "#FFFF99")
     g.addColorStop(1, "#000000");
@@ -71,8 +64,8 @@ class Planet extends StellarObject {
     super();
 
     // known parameters of an elliptic orbit found in the NASA database
-    this.ecc = 0.75;
-    this.semiMajor = Math.floor(Math.random() * 480);
+    this.ecc = 0.5;
+    this.semiMajor = Math.floor(Math.random() * 240);
     this.semiMinor = Math.sqrt(Math.pow(this.semiMajor, 2) * (1 - Math.pow(this.ecc, 2)));
 
     //foci
@@ -80,7 +73,7 @@ class Planet extends StellarObject {
 
     // Give the planet a random theta and velocity
     this.theta = Math.random() * 2 * Math.PI;
-    this.dtheta = Math.random() / 25;
+    this.dtheta = Math.random() / 20  ;
     // this.radius = Math.floor(Math.random() * 240) + 32;
 
     // calculate radius from a given (initially random) theta and some terrifying crazy ellipse math
@@ -90,7 +83,7 @@ class Planet extends StellarObject {
     this.x = (Math.cos(this.theta) * this.radius) + 320 + this.foci;
     this.y = (Math.sin(this.theta) * this.radius) + 320;
 
-    this.trailLength = 64;
+    this.trailLength = 128;
     this.trailPositions = [];
   }
 
@@ -109,10 +102,11 @@ class Planet extends StellarObject {
   drawOrbit(r) {
     ctx.strokeStyle = "#3F3F3F";
     ctx.beginPath();
-    ctx.ellipse(320, 320, this.semiMajor, this.semiMinor, 0, 0, 2 * Math.PI, false)
+    ctx.ellipse(320 + this.foci, 320, this.semiMajor, this.semiMinor, 0, 0, 2 * Math.PI, false)
     ctx.stroke();
   }
 
+  // Use the trailPositions array to store
   drawTrail(){
     let opacity = 0;
 
@@ -127,20 +121,22 @@ class Planet extends StellarObject {
     }
   }
 
+  // Update the "numbers" before drawing
   update(){
     if (this.theta >= (2 * Math.PI)) this.theta = 0;
 
+    // Push the current x-y position into the array to generate a motion trail in drawTrail() function
+    // Also, take the last planet motion trail particle out of the array with shift()
     this.trailPositions.push({x: this.x, y: this.y});
     if (this.trailPositions.length > this.trailLength) this.trailPositions.shift();
 
-    this.x = (Math.cos(this.theta) * this.radius) + 320 + this.foci;
+    // convert polar coordinates (radius and theta) to Cartesian canvas coordinates (x, y)
+    this.x = (Math.cos(this.theta) * this.radius) + 320 + (2 * this.foci);
     this.y = (Math.sin(this.theta) * this.radius) + 320;
 
+    // update theta for the next draw() pass; the angle produces acceleration effect AND radius is not available until theta is set
     this.theta += this.dtheta;
-    // this.radius = this.semiMajor * (1 - Math.pow(this.ecc, 2)) / (1 + Math.cos(this.theta));
     this.radius = this.semiMajor * (1 - Math.pow(this.ecc, 2)) / (1 + (this.ecc * Math.cos(this.theta)))
-
-    console.log(this.semiMajor);
   }
 }
 
