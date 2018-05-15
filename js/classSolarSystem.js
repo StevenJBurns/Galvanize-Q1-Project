d7b1411d149fd448800a727b3b52933ba9e9c47e
SJB <StevenJBurns@users.noreply.github.com>
2018-05-14 21:23:22 -0600

    Copied original SolarSystem class into this module as an export

--- js/classSolarSystem.js
+++ js/classSolarSystem.js
@@ -1,1 +1,10 @@
 "use strict";
+
+export class SolarSystem {
+  constructor(name, dist, planetCount) {
+    this.systemName = name || "";
+    this.distanceFromEarth = dist || 0;
+    this.planetCount = planetCount || 0;
+    this.star = {};
+    this.planets = [];
+}
