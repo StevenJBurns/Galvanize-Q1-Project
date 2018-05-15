f765c5e3432705e803b60f5ab53c15dddeeff151
SJB <StevenJBurns@users.noreply.github.com>
2018-05-14 21:27:43 -0600

    Copied original Star class into this module

--- js/classStar.js
+++ js/classStar.js
@@ -1,1 +1,20 @@
 "use strict";
+
+export class Star {
+  constructor(binary, mass, radius, temperature) {
+    this.isBinary = binary;
+    this.mass = mass || 0;
+    this.radius = radius || 0;
+    this.temperature = temperature;
+    this.planets = [];
+
+    // For binaryDraw() method only !!!
+    this.thetaA = (Math.random() * Math.PI) + Math.PI;
+    this.thetaB = this.thetaA - Math.PI;
+    this.dtheta = 0.0075;
+
+    this.polarRadiusA = 24;
+    this.polarRadiusB = 32;
+    this.xA, this.yA, this.xB, this.yB;
+  }
+}
